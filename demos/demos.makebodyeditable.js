9016d6b532ce7d5e6b6f6fa86eb7a1b053bf725e
Ganesh B <ganeshsurfs@gmail.com>
2024-03-03 05:27:27 +0530

    [COMMIT] First inbrowser editing function commit

---
+++ demos/demos.makebodyeditable.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: inbrowseredit
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i inbrowseredit --save
+ * Github: https://github.com/ganeshkbhat/inbrowseredit
+ * npmjs Link: https://www.npmjs.com/package/inbrowseredit
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Package: inbrowseredit
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i inbrowseredit --save
+ * Github: https://github.com/ganeshkbhat/inbrowseredit
+ * npmjs Link: https://www.npmjs.com/package/inbrowseredit
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+function EE() {
+
+  /**
+   *
+   *
+   */
+  function makeBodyEditable() {
+    document.body.contentEditable = true;
+  }
+
+  /**
+   *
+   *
+   */
+  function makeEditable() {
+    document.designMode = 'on';
+  }
+
+  /**
+   *
+   *
+   */
+  function disableBodyEditing() {
+    document.body.contentEditable = true;
+  }
+
+  /**
+   *
+   *
+   */
+  function disableEditing() {
+    document.designMode = 'off';
+  }
+
+  return {
+    makeBodyEditable,
+    makeEditable,
+    disableBodyEditing,
+    disableEditing
+  }
+}
